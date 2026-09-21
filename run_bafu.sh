5b41c4e947c2dfca88f8011bec3c1203d4ab2e29
LouisFreboeuf <louis.freboeuf@gmail.com>
2026-09-21 11:08:49 +0200

    change project name to "reverse-bafu"

--- run_bafu.sh
+++ run_bafu.sh
@@ -1,4 +1,4 @@
-uv run sentier-brightway db --project NAME
+uv run sentier-brightway db --project reverse-bafu
 uv run sentier-brightway files --out DIR
 uv run sentier-brightway coverage
 uv run sentier-brightway backtest --out DIR
